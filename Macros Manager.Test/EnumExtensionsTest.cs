1062153aed180e69a65246c9f4ce11d7ed1035a8
Mikus Miroslav <miroslav.mikus@outlook.com>
2017-09-12 19:42:08 +0200

    Add simple test for EnumExtensions

---
+++ Macros Manager.Test/EnumExtensionsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Macros_Manager.Tools;
+using Macros_Manager.Unity.Enums;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Macros_Manager.Test
+{
+    [TestClass]
+    public class EnumExtensionsTest
+    {
+        [TestMethod]
+        public void TestEnumToList()
+        {
+            // arrange
+            List<string> expected = new List<string>()
+            {
+                NodeType.Label.ToString(),
+                NodeType.Macro.ToString(),
+                NodeType.Sequence.ToString(),
+                NodeType.Dashboard.ToString(),
+                NodeType.Data.ToString(),
+            };
+
+            // act
+            var result = EnumExtensions.EnumToList(typeof(NodeType));
+
+            // assert
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
